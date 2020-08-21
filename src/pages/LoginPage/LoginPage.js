043c6fcd8e31beaf947200b95c1dfcd4e8502446
Brian Kelly <briankelly@Brians-Mac-mini.local>
2020-08-21 14:51:36 -0700

    configure link in LoginPage

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './LoginPage.css';
 
-const LoginPage = () => {
+class LoginPage extends Component {
     render() {
         return (
             <div classname="LoginPage">
