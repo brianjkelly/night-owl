6a1a120a305d75ffbcedccb9dae84d500f5fa4a0
adferrin <adferrin@icloud.com>
2020-08-21 14:46:25 -0700

    update to state

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -4,11 +4,14 @@ import { Route, Switch } from 'react-router-dom';
 
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
+import userService from '../../../utils/userService'
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      user: userService.getUser(),
+    }
   }
   render() {
     return (
