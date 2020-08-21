301e083db27b1d3b6a227e0e3cfa158dbf3c7d85
Brian Kelly <briankelly@Brians-Mac-mini.local>
2020-08-21 14:00:01 -0700

    configure router for LandingPage

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+
+import LandingPage from '../LandingPage/LandingPage';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          videoRange
-        </header>
+        <header className="App-header">videoRange</header>
+        <Route exact path='/' render={() =>
+          <LandingPage />
+        } />
       </div>
     );
   }
