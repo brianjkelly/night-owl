7dd9dcf483827d7971407a1dff087e9eac4aaaf0
Brian Kelly <briankelly@Brians-Mac-mini.local>
2020-08-25 11:45:21 -0700

    configure back end controller for api

--- routes/api/videos.js
+++ routes/api/videos.js
@@ -2,22 +2,10 @@ const express = require('express');
 const router = express.Router();
 const request = require('request');
 const { google } = require('googleapis');
+const videoCtrl = require('../../controllers/videos');
 
 
-// key = process.env.API_KEY
-
-// const rootURL = 'https://www.googleapis.com/youtube/v3/';
-
-// const params = {
-//     part: "snippet",
-//     maxResults: 5,
-//     id: { kind: "youtube#video" },
-//     type: "video",
-//     videoDefinition: "high",
-//     videoEmbeddable: "true",
-//     order: "viewCount",
-    // key: process.env.API_KEY
-// }
+router.get('/search', videoCtrl.search);
 
 
 module.exports = router;
