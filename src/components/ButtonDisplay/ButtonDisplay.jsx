250354dcd2102f777203e3ad15e8f1441fa90430
Brian Kelly <briankelly@Brians-Mac-mini.local>
2020-08-27 17:17:07 -0700

    add margin to video items

---
+++ src/components/ButtonDisplay/ButtonDisplay.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PlayButton from '../PlayButton/PlayButton';
+import QuButton from '../QuButton/QuButton';
+import RemoveButton from '../RemoveButton/RemoveButton';
+
+const ButtonDisplay = (props) => {
+
+    return (
+
+        <div className="all-buttons">
+            <div className="play-btn">
+                <PlayButton 
+                    handlePlayBtn={props.handlePlayBtn}
+                />
+            </div>
+            <div className="add-btn">
+                <QuButton 
+                    handleAddToQ={props.handleAddToQ}
+                />
+            </div>
+            <div className="rmv-btn">
+                <RemoveButton 
+                    handleRemoveFromQ={props.handleRemoveFromQ} 
+                />
+            </div>
+        </div>
+
+    );
+}
+
+export default ButtonDisplay;
